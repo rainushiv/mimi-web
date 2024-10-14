f57b4aeddb56309b4b684304703457458ab00ea1
rainushiv <shivpreet567@gmail.com>
2024-10-14 01:24:39 -0400

    init

--- src/Shared/hooks/config.js
+++ src/Shared/hooks/config.js
@@ -1,2 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL ?? "http://localhost:4000";
 
+require('dotenv').config()
+
+export const API_URL = process.env.REACT_APP_API_URL ?? "http://localhost:4000";

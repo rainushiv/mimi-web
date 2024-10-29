606a8a8d1bce9b52678e2421b2e797b92503c799
rainushiv <shivpreet567@gmail.com>
2024-10-29 12:10:52 -0400

    init

--- src/Home/Components/HomeContentText.jsx
+++ src/Home/Components/HomeContentText.jsx
@@ -5,7 +5,7 @@ export default function HomeContentText() {
     return (
         <div className="HomeContentText-Container">
             <h1>About Us</h1>
-            <p> THIS IS NEW ipsum dolor sit amet consectetur adipisicing elit. Quo quas quam quidem dicta ratione, sint ducimus maiores ipsam! Rerum nemo laborum exercitationem, quas error ad qui odit sed placeat illo.</p>
+            <p>Its literally happening ipsum dolor sit amet consectetur adipisicing elit. Quo quas quam quidem dicta ratione, sint ducimus maiores ipsam! Rerum nemo laborum exercitationem, quas error ad qui odit sed placeat illo.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas quam quidem dicta ratione, sint ducimus maiores ipsam! Rerum nemo laborum exercitationem, quas error ad qui odit sed placeat illo.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas quam quidem dicta ratione, sint ducimus maiores ipsam! Rerum nemo laborum exercitationem, quas error ad qui odit sed placeat illo.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quas quam quidem dicta ratione, sint ducimus maiores ipsam! Rerum nemo laborum exercitationem, quas error ad qui odit sed placeat illo.</p>
